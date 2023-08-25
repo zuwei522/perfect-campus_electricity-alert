5caa73955780585bc58eca8505f14f64ffe8b1dd
Zuwei Jiang <2338096031@qq.com>
2023-08-26 00:26:38 +0800

    正式发布

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# perfect-campus_electricity-alert
-完美校园⏰用电提醒
+# 完美校园 低电费提醒🔔
+
+你是否曾经因为忘记检查电费，而遭遇晚上断电，还恰好处于充值系统的维护时间？ /(ㄒoㄒ)/~~  
+于是，为了避免悲剧再次发生，这个**使用 Github Action** 定时执行、借助 [Qmsg 酱](https://qmsg.zendee.cn/)发送低电费提醒的 **shell 脚本**就诞生了！ （好耶ヽ(✿ﾟ▽ﾟ)ノ
+
+## 工作原理
+
+- 本脚本默认在每天 7:00-23:59 的每个整点，自动查询在完美校园上所绑定的所有房间或空调的剩余度数，若低于设定的阈值，就通过 Qmsg 酱向指定 QQ 推送消息。
+
+## 开始使用
+
+  1. Fork 本仓库到自己的名下
+  2. 打开新 Fork 的仓库主页，依次进入👉 `Settings` → `Secrets and variables` → `Actions`
+  3. 点击右侧绿色按钮 `New repository secret`，按照下表依次填入 Name 和 Secret
+
+      | Name       | Secret |
+      | ---------- | ------ |
+      | QMSG_KEY   | [点我登录 Qmsg 酱控制台](https://qmsg.zendee.cn/user)，找到并填入「我的KEY」。选择并主动添加可用的 Qmsg 酱的QQ好友，即可接收到 bot 的消息推送。|
+      | ALERT_QQ   | 填入你的 QQ 号 |
+      | SCHOOL_ID  | 填入你所在学校的编码👉 [点我查看学校编码](./school-list.md) |
+      | STUDENT_ID | 填入你的学号 |
+
+  4. 添加完上述 secret 后，再点击绿色按钮 `New repository secret` 左边的 `Variables` 选项卡，然后点击右侧绿色按钮 `New repository variable`，按照下表依次填入 Name 和 Value
+
+      | Name            | Value |
+      | --------------- | ----- |
+      | ALERT_THRESHOLD | 填入你想要设置的提醒阈值，单位是度，例如：15 |
+
+  5. 回到仓库主页，依次进入👉 `Actions` → `完美校园低电费提醒`，点击右侧 `Run workflow` 可手动立即运行一次。若暂时不需要提醒，可在该页面的 `Filter workflow runs` 搜索框右边的 `···` 按钮中选择 `Disable workflow`，暂时禁用这个 workflow
+
+## 注意事项
+
+  1. 完美校园的智能水电偶尔会抽风，表现为手机 APP 无法进入相关页面，同时本脚本也无法查询相关信息。
+  
+  2. 本脚本消息推送功能使用的是[公共版 Qmsg 酱](https://qmsg.zendee.cn/docs/start/#%E5%85%AC%E5%85%B1%E7%89%88-%E6%8D%90%E8%B5%A0%E7%89%88-plus%E7%89%88-%E5%B7%AE%E5%BC%82)，消息推送数量上限为30条/天。
+  
+  3. 若需修改定时执行频率或时间段，请修改文件 [.github/workflows/backup.yml](./.github/workflows/main.yml) 第 4 行 `cron` 引号内的 [cron 表达式](https://zhuanlan.zhihu.com/p/437328366)。
+  
+  4. 桂电北海校区经[测试](https://github.com/zuwei522/perfect-campus_electricity-alert/actions/runs/5976106345/job/16213315269)可正常使用，其他学校暂不清楚能不能用。可在本仓库的 [Discussions](https://github.com/zuwei522/perfect-campus_electricity-alert/discussions/categories/%E5%90%84%E5%AD%A6%E6%A0%A1%E5%8F%AF%E7%94%A8%E6%80%A7%E5%88%86%E4%BA%AB) 分享你的或查看他人的测试结果。
+
+      | Qmsg 推送消息 | 完美校园智能水电查询截图 |
+      |:-:|:-:|
+      | ![Qmsg消息截图](./images/Qmsg.png) | ![完美校园智能水电截图](./images/Screenshot_2023-08-26-00-01-13-621_com.newcapec.m.jpg) |
+
+## 如果觉得不错，可以点个⭐Star 吗？
+
+![求求你了](./images/求求你了.jpg)
--- main.sh
+++ main.sh
@@ -1,61 +1,58 @@
 #!/bin/bash
-
-# SCHOOL_ID= #学校编号
-# STUDENT_ID=   #学号
-# ALERT_QQ=   #与学号对应的QQ
-# QMSG_KEY= #与学号对应的推送Key 获取方法：https://qmsg.zendee.cn/user 登录Qmsg控制台即可获取我的KEY，选择并添加可用的Qmsg酱的QQ好友，即可接收到消息推送
-# ALERT_THRESHOLD=    #低电量提醒阈值
-
-body=$(curl -sd "param=%7B%22cmd%22%3A%22getbindroom%22%2C%22account%22%3A%22${STUDENT_ID}%22%7D&customercode=${SCHOOL_ID}&method=getbindroom" "https://xqh5.17wanxiao.com/smartWaterAndElectricityService/SWAEServlet" | jq .body)
-    body=$(echo $(echo $(echo $body | sed 's/"{/{/g') | sed 's/}"/}/g') | sed 's/\\"/"/g')  #手动整理json格式
-    roomAmount=$(echo $body | jq '.roomlist|length')    #获取绑定的房间数量
-
-    for((i=0;i<$roomAmount;i++))
-    do
-        roomName[$i]=$(echo $(echo $body | jq .roomlist[$i].roomfullname) | sed 's/"//g')   #房间名
-        roomUse[$i]=$(echo $(echo $body | jq .roomlist[$i].detaillist[0].use) | sed 's/"//g')   #已使用电量
-        roomOdd[$i]=$(echo $(echo $body | jq .roomlist[$i].detaillist[0].odd) | sed 's/"//g')   #剩余电量
-        roomStatusCode[$i]=$(echo $(echo $body | jq .roomlist[$i].detaillist[0].status) | sed 's/"//g') #状态码
-        if [ ${roomStatusCode[$i]} -eq 1 ];then #将状态码转换为字符
-            roomStatus[$i]="一般送电"
-        else
-            roomStatus[$i]="一般断电"
-        fi
-    done
     
-    msg="【电费不足提醒】目前与您的学号${STUDENT_ID:0:2}*****${STUDENT_ID:7:5}绑定的以下房间，剩余电量不足${ALERT_THRESHOLD}度，请及时缴纳电费哦~"
-    msgFlag=0
-    for((i=0;i<$roomAmount;i++))
-    do
-        if [ $(printf "%.0f" ${roomOdd[$i]}) -lt ${ALERT_THRESHOLD} ];then
-            msgFlag=1
-            msg="$msg   【$(echo ${roomName[$i]} | sed 's/公寓/宿舍/g')】剩余${roomOdd[$i]}度电"
-        fi
-    done
+# SCHOOL_ID=        #学校编号
+# STUDENT_ID=       #学号
+# ALERT_QQ=         #要推送的 QQ
+# QMSG_KEY=         # Qmsg 酱推送Key 获取方法：https://qmsg.zendee.cn/user 登录Qmsg控制台即可获取我的KEY，选择并添加可用的Qmsg酱的QQ好友，即可接收到消息推送
+# ALERT_THRESHOLD=  #低电量提醒阈值
 
-    #输出日志
-    echo -e "[`date "+%Y-%m-%d %A %H:%M:%S"`] 学号${STUDENT_ID:0:2}*****${STUDENT_ID:7:5}共绑定了$roomAmount个房间\n房间\t\t\t已使用\t剩余\t状态"
-    for((i=0;i<$roomAmount;i++))
-    do
-        echo -e "${roomName[$i]}\t${roomUse[$i]}\t${roomOdd[$i]}\t${roomStatus[$i]}"
-    done
+body=$(curl -sd "param=%7B%22cmd%22%3A%22getbindroom%22%2C%22account%22%3A%22${STUDENT_ID}%22%7D&customercode=${SCHOOL_ID}&method=getbindroom" "https://xqh5.17wanxiao.com/smartWaterAndElectricityService/SWAEServlet" | jq .body) #从完美校园获取信息
+body=$(echo $(echo $(echo $body | sed 's/"{/{/g') | sed 's/}"/}/g') | sed 's/\\"/"/g')  #手动整理 json 格式
+roomAmount=$(echo $body | jq '.roomlist|length')    #获取绑定的房间数量
 
-    # if [ $msgFlag -eq 1 ];then
-    QmsgFlag=false
-    i=0
-    while [ $msgFlag -eq 1 ] && [ "$QmsgFlag" != "true" ] && [ $i -lt 3 ]
-    do
-        echo $(echo "向与学号 ${STUDENT_ID:0:2}*****${STUDENT_ID:10:2} 绑定的QQ号 ${ALERT_QQ:0:3}******** 发送消息：$msg")
-        res=$(curl -sd "qq=${ALERT_QQ}&msg=$msg" "https://qmsg.zendee.cn:443/send/${QMSG_KEY}")
-        QmsgFlag=$(echo $res | jq .success)
-        if [ "$QmsgFlag" == "true" ];then
-            echo "发送成功：$res"
-        else
-            echo "发送失败：$res"
-            sleep 3
-        fi
-        # sleep 3
-        let i++
-    done
+for((i=0;i<$roomAmount;i++))    #依次提取 json 中的数据
+do
+    roomName[$i]=$(echo $(echo $body | jq .roomlist[$i].roomfullname) | sed 's/"//g')   #房间名
+    roomUse[$i]=$(echo $(echo $body | jq .roomlist[$i].detaillist[0].use) | sed 's/"//g')   #已使用电量
+    roomOdd[$i]=$(echo $(echo $body | jq .roomlist[$i].detaillist[0].odd) | sed 's/"//g')   #剩余电量
+    roomStatusCode[$i]=$(echo $(echo $body | jq .roomlist[$i].detaillist[0].status) | sed 's/"//g') #状态码
+    if [ ${roomStatusCode[$i]} -eq 1 ];then #将状态码转换为字符
+        roomStatus[$i]="一般送电"
+    else
+        roomStatus[$i]="一般断电"
+    fi
+done
+    
+msg="【电费不足提醒】目前与您的学号 ${STUDENT_ID:0:4}****** 绑定的以下房间，剩余电量不足 ${ALERT_THRESHOLD} 度，请及时缴纳电费哦~"
+msgFlag=0   #是否需要推送消息标记
+for((i=0;i<$roomAmount;i++))
+do
+    if [ $(printf "%.0f" ${roomOdd[$i]}) -lt ${ALERT_THRESHOLD} ];then  #判断是否低于阈值
+        msgFlag=1
+        msg="$msg   【$(echo ${roomName[$i]} | sed 's/公寓/宿舍/g')】剩余${roomOdd[$i]}度电"
+    fi
+done
 
-    echo
+#输出日志
+echo -e "[`date "+%Y-%m-%d %A %H:%M:%S"`] 学号${STUDENT_ID:0:2}*****${STUDENT_ID:7:5}共绑定了$roomAmount个房间\n房间\t\t\t已使用\t剩余\t状态"
+for((i=0;i<$roomAmount;i++))
+do
+    echo -e "${roomName[$i]}\t${roomUse[$i]}\t${roomOdd[$i]}\t${roomStatus[$i]}"
+done
+
+QmsgFlag=false  #是否推送成功标记
+i=0
+while [ $msgFlag -eq 1 ] && [ "$QmsgFlag" != "true" ] && [ $i -lt 3 ]   #若第一次推送失败，只重试 2 次
+do
+    echo $(echo "向与学号 ${STUDENT_ID:0:2}*****${STUDENT_ID:10:2} 绑定的QQ号 ${ALERT_QQ:0:3}******** 发送消息：$msg")
+    res=$(curl -sd "qq=${ALERT_QQ}&msg=$msg" "https://qmsg.zendee.cn:443/send/${QMSG_KEY}")
+    QmsgFlag=$(echo $res | jq .success)
+    if [ "$QmsgFlag" == "true" ];then   #输出是否推送成功日志
+        echo "发送成功：$res"
+    else
+        echo "发送失败：$res"
+        sleep 3
+    fi
+    # sleep 3
+    let i++
+done
